9914999d7aa00ab1f4c90bd5e25f3008c38f174b
nahye <nnhh.shine@gmail.com>
2025-01-12 15:18:17 +0900

    [mvc1] 섹션7. 스프링 MVC-기본기능 - HTTP 응답-HTTP API, 메시지 바디에 직접 입력

--- springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-public class ResponseViesController {
+public class ResponseViewController {
 
     @RequestMapping("/response-view-v1")
     public ModelAndView responseViewV1() {
