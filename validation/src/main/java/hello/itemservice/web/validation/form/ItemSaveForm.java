2772f69c7a4cf6c0a11e4d59604500ef9f32c3b4
nahye <nnhh.shine@gmail.com>
2025-01-19 19:13:07 +0900

    [mvc2] 섹션6. 검증2 - Form 전송 객체 분리-개발

--- validation/src/main/java/hello/itemservice/web/validation/form/ItemSaveForm.java
+++ validation/src/main/java/hello/itemservice/web/validation/form/ItemSaveForm.java
@@ -1,4 +1,4 @@
-package hello.itemservice.domain.item;
+package hello.itemservice.web.validation.form;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
@@ -7,19 +7,17 @@ import lombok.Data;
 import org.hibernate.validator.constraints.Range;
 
 @Data
-public class Item {
+public class ItemSaveForm {
 
-    private Long id;
+    @NotBlank
     private String itemName;
+
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    @NotNull
+    @Max(value = 9999)
     private Integer quantity;
 
-    public Item() {
-    }
-
-    public Item(String itemName, Integer price, Integer quantity) {
-        this.itemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
-    }
 }
--- validation/src/main/java/hello/itemservice/web/validation/form/ItemUpdateForm.java
+++ validation/src/main/java/hello/itemservice/web/validation/form/ItemUpdateForm.java
@@ -1,4 +1,4 @@
-package hello.itemservice.domain.item;
+package hello.itemservice.web.validation.form;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
@@ -7,19 +7,19 @@ import lombok.Data;
 import org.hibernate.validator.constraints.Range;
 
 @Data
-public class Item {
+public class ItemUpdateForm {
 
+    @NotNull
     private Long id;
+
+    @NotBlank
     private String itemName;
+    
+    @NotNull
+    @Range(min = 1000, max = 1000000)
     private Integer price;
+
+    // 수정시 수량은 자유롭게 변경할 수 있다.
     private Integer quantity;
 
-    public Item() {
-    }
-
-    public Item(String itemName, Integer price, Integer quantity) {
-        this.itemName = itemName;
-        this.price = price;
-        this.quantity = quantity;
-    }
 }
