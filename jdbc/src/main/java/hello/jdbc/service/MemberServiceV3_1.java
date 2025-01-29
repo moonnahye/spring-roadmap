70aa30786ca4f27e821213c3ec17bd9885e26323
nahye <nnhh.shine@gmail.com>
2025-01-29 16:55:03 +0900

    [db1] 섹션5.스프링과 문제 해결-트랙잭션-트랜잭션 문제 해결-트랜잭션 매니저1

---
+++ jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -0,0 +1,50 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+import java.sql.SQLException;
+
+/**
+ * 트랜잭션 - 트랜잭션 매니저
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV3_1 {
+
+    private final PlatformTransactionManager transactionManager;
+    private final MemberRepositoryV3 memberRepository;
+
+    public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+        // 트랜잭션 시작
+        TransactionStatus status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+
+        try {
+            bizLogic(toId, money, fromId);
+            transactionManager.commit(status); // 성공시 커밋
+        } catch (Exception e) {
+            transactionManager.rollback(status); // 실패하면 롤백
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    private void bizLogic(String toId, int money, String fromId) throws SQLException {
+        Member fromMember = memberRepository.findById(fromId);
+        Member toMember = memberRepository.findById(toId);
+
+        memberRepository.update(fromId, fromMember.getMoney() - money);
+        validation(toMember);
+        memberRepository.update(toId, toMember.getMoney() + money);
+    }
+
+    private void validation(Member toMember) {
+        if (toMember.getMemberId().equals("ex")) {
+            throw new IllegalArgumentException("이체증 예외 발생");
+        }
+    }
+}
